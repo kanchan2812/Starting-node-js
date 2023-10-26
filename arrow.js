b8be604aef015865051a7ba006241cf55e4798fc
kanchan2812 <dighekanchan4@gmail.com>
2023-10-26 14:49:36 +0530

    task-2 added arrow function + student object

---
+++ arrow.js
@@ -0,0 +1,5 @@
+const product=(a,b)=>{
+    ans=a*b;
+    return ans;
+}
+console.log(product(4,5));
---
+++ studentObject.js
@@ -0,0 +1,9 @@
+const student={
+name:'kanchan',
+age:29,
+greet(){
+    console.log('hi, i am '+this.name);
+}
+};
+console.log(student.name);
+student.greet();
